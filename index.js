4c538606306c20f2a3b44fa47df4a84f350346c3
John McLear <john@mclear.co.uk>
2016-04-09 20:02:22 +0100

    export working for spans, needs more work for lines but many progress

--- index.js
+++ index.js
@@ -61,6 +61,8 @@ exports.aceAttribClasses = function(hook_name, attr, cb){
 */
 
 // Include CSS for HTML export
+// Also does the load from the API if we don't already have the context info!
+// CAKE STILL TO DO: Madison needs to accept this request as it wont have the correct cookie info!
 exports.stylesForExport = function(hook, padId, cb){
   if(!contexts[padId]){
     loadPadContexts(padId, null, function(){
@@ -79,9 +81,15 @@ exports.exportHtmlAdditionalTags = function(hook, pad, cb){
 
 // line, apool,attribLine,text
 exports.getLineHTMLForExport = function (hook, line) {
+  var lineContent = rewriteLine(line);
+  return lineContent;
+  // CAKE TO DO STill process line stuff as below
+
+  var lineContent = line.lineContent;
+  // TODO: when "asyncLineHTMLForExport" hook is available on Etherpad, return "lineContent" instead of re-setting it
+  line.lineContent = lineContent;
 
   /*
-
   var contextV = _analyzeLine(line.attribLine, line.apool);
 
   // If it has a context
@@ -95,9 +103,9 @@ exports.getLineHTMLForExport = function (hook, line) {
   var after = "";
 
   // we prolly need to do a htt request for contexts here..
-
-  if (contexts.length) {
-    contexts.forEach(function(contextV){
+  // CAKE here we need the padId!
+  if (contexts[padId].array.length) {
+    contexts[padId].array.forEach(function(contextV){
       if(contextV.indexOf("context") !== 0){
         before += "<p class='context context" + contextV + "'><span class='contextbefore'>";
       }else{
@@ -142,6 +150,7 @@ exports.getLineHTMLForExport = function (hook, line) {
 // TODO Use Server side logic not hard coded
 exports.exportHTMLSend = function(hook, html, cb){
   /*
+  console.warn("hook", hook);
   var blockElements = ["Sponsor", "Title", "Whereas", "Resolved", "Signature", "Date", "LastWhereas", "LastResolved", "FirstResolved"];
   console.warn("um okay");
   sanitize.exec(html, blockElements, function(error, cleanedHTML){
@@ -206,7 +215,7 @@ exports.expressCreateServer = function (hook_name, args, callback) {
     var splitPath = path.split("/");
     var padId= splitPath[4];
     var padURL = req.protocol + "://" + req.get('host') + "/p/" +padId;
-    loadPadContexts(padId, req, function(){
+    loadPadContexts(padId, req, function(styles){
       res.send("var contexts = " +JSON.stringify(styles));
     });
   });
@@ -252,28 +261,21 @@ function getAsUriParameters(data) {
   return url.substring(0, url.length - 1)
 }
 
-exports.asyncLineHTMLForExport = function (hook, context, cb) {
-  console.warn("here");
-  cb(rewriteLine);
-}
-
-function rewriteLine(context){
-  var lineContent = context.lineContent;
+function rewriteLine(hook){
+  var lineContent = hook.lineContent;
+  var padId = hook.padId;
   // WTF this never runs either on either plugin!
-  console.warn("lineContent", lineContent);
-/*
-  sizes.forEach(function(size){
-    size = size.replace("fs","");
+  contexts[padId].array.forEach(function(context){
+    context = context.replace("context:","");
     if(lineContent){
-      lineContent = lineContent.replaceAll("<fs"+size, "<span style='font-size:"+size+"px'");
-      lineContent = lineContent.replaceAll("</fs"+size, "</span");
+      lineContent = lineContent.replaceAll("<context:"+context, "<span class='context"+context +"'");
+      lineContent = lineContent.replaceAll("</context:"+context, "</span");
     }
   });
-*/
   return lineContent;
 }
 
 String.prototype.replaceAll = function(str1, str2, ignore) 
 {
   return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
-} 
+}
